f237b7a344f92ed7a342940296bcc02c150d9944
jezandroid <hillman.jeremy@gmail.com>
2016-09-03 23:39:24 +0100

    Add files via upload

--- www/js/index.js
+++ www/js/index.js
@@ -56,3 +56,18 @@ function onSuccess(a){
 }
 function onError(){}
 
+// function progress(percent, $element) {
+//     var progressBarWidth = percent * $element.width() / 100;
+//     $element.find('div').animate({ width: progressBarWidth }, 500).html(percent + "% ");
+// }
+
+function progress(timeleft, timetotal, $element) {
+    var progressBarWidth = timeleft * $element.width() / timetotal;
+    $element.find('div').animate({ width: progressBarWidth }, 500).html(timeleft + " seconds to go");
+    if(timeleft > 0) {
+        setTimeout(function() {
+            progress(timeleft - 1, timetotal, $element);
+        }, 1000);
+    }
+};
+
